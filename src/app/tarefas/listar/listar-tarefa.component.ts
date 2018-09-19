e1f14a3c4ae8bb94ef56fe89007c7b6660fa4359
raylison100 <raylison100@gmail.com>
2018-09-19 15:14:03 -0300

    Adicionado component Editar

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {TarefaService, Tarefa} from "../shared";
+import {$} from "protractor";
 
 @Component({
   selector: 'app-listar-tarefa',
@@ -19,5 +20,20 @@ export class ListarTarefaComponent implements OnInit {
 
   listarTodos(): Tarefa[]{
     return this.tarefaService.listarTodos();
+  }
+
+  remover($event: any, tarefa: Tarefa): void {
+    $event.preventDefault();
+    if (confirm('Deseja remover a tarefa "' + tarefa.nome +'"?')){
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();
+    }
+  }
+
+  alterarStatus(tarefa: Tarefa): void {
+    if (confirm('Deseja alterar o status da tarefa "' + tarefa.nome +'"?')) {
+      this.tarefaService.alterarStatus(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();
+    }
   }
 }
